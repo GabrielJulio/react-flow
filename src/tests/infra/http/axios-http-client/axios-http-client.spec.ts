30f3a25748a02d335645ae3e30901a147d7e90ba
Gabriel Júlio <gabrieljuliobs@gmail.com>
2021-09-21 16:44:27 -0300

    Garantido que o AxiosHttpClient chama a url com verbo correos com o axios

--- src/tests/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/tests/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -11,11 +11,11 @@ jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct URL', async () => {
+  test('Should call axios with correct URL and verb', async () => {
     const url = faker.internet.url()
     const sut = makeSut()
     sut.post({ url })
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
     
   })
   
