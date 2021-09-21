0235e3260d769423da4e121426878fb351e0df10
Gabriel Júlio <gabrieljuliobs@gmail.com>
2021-09-21 12:51:46 -0300

    Garantido que o RemoteAuthentication chame o HttpPostClient com o body correto

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,4 +1,5 @@
-import { HttpPostClient } from "data/protocols/http/http-post-client";
+import { HttpPostClient } from "data/protocols/http/http-post-client"
+import { AuthenticationParams } from "domain/usecases/authentication"
 
 
 export class RemoteAuthentication {
@@ -7,9 +8,10 @@ export class RemoteAuthentication {
     private readonly httpPostClient: HttpPostClient
     ) {}
 
-  async auth(): Promise<void> {
+  async auth(params: AuthenticationParams): Promise<void> {
     await this.httpPostClient.post({
-      url: this.url
+      url: this.url,
+      body: params
     })
   }
 }
